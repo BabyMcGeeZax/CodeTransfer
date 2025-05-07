a99dd23ec52e42e51663b6c27d1a1fec93c347b1
BabyMcGeeZax <154984452+BabyMcGeeZax@users.noreply.github.com>
2025-05-07 08:33:03 +0000

    print out gcd s

--- p.cpp
+++ p.cpp
@@ -10,25 +10,14 @@ int gcd(int a, int b) {
     return a;
 }
 
-bool areCoprime(int a, int b) {
-    return gcd(a, b) == 1;
-}
-
 int main() {
     int n;
-    std::cout << "Enter number of pairs: ";
+    std::cout << "Print all numbers coprime to: ";
     std::cin >> n;
 
     for (int i = 0; i < n; ++i) {
-        int x, y;
-        std::cout << "Enter pair " << i + 1 << " (x y): ";
-        std::cin >> x >> y;
-
-        if (areCoprime(x, y)) {
-            std::cout << x << " and " << y << " are coprime.\n";
-        } else {
-            std::cout << x << " and " << y << " are not coprime.\n";
-        }
+        gcd(i,n);
+        cout << gcd(i,n) << endl;
     }
 
     return 0;
